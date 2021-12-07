acd5b53755bc9865b5ccb28d789a8797a0504d3d
peterhpchen <peterhsinpingchen@gmail.com>
2021-12-07 21:51:50 +0800

    finish first try

--- experiment/matrixMultiplicationExperiment.ts
+++ experiment/matrixMultiplicationExperiment.ts
@@ -1,12 +1,23 @@
 const fs = require("fs");
 const { matrixMultiplicationStrassen } = require("./matrixMultiplication.ts");
 
-const maxN = 256;
-const gap = 32;
-const round = 10;
-// const maxN = 2048;
-// const gap = 256;
+// const gap = 16;
 // const round = 1;
+// const maxN = 1440;
+// const nList = Array.from(Array(maxN / gap).keys()).map(
+//   (val) => (val + 1) * gap
+// );
+// const tList = [8, 1440]
+// first-try
+const gap = 16;
+const round = 10;
+const maxN = 512;
+const nList = Array.from(Array(maxN / gap).keys()).map(
+  (val) => (val + 1) * gap
+);
+const tList = Array.from(Array(maxN / gap + 1).keys()).map(
+  (val) => val * gap || 1
+);
 
 // const simpleStart = performance.now();
 // matrixMultiplicationSimple(n, n, n, matrix1, matrix2);
@@ -19,11 +30,11 @@ const round = 10;
 // console.log("Strassen", strassenEnd - strassenStart);
 
 const result = {
-  labels: Array.from(Array(maxN / gap).keys()).map((val) => (val + 1) * gap),
+  labels: nList,
   datasets: [],
 };
 
-for (let n = gap; n <= maxN; n += gap) {
+nList.forEach((n) => {
   // full matrix
   const matrix1 = [...Array(n)].map(() =>
     [...Array(n)].map(() => Math.ceil(Math.random() * 1000))
@@ -40,31 +51,29 @@ for (let n = gap; n <= maxN; n += gap) {
   // );
 
   // ["1", "n/64", "n/32", "n/16", "n/8", "n/4", "n/2", "n"].forEach((t, ti) => {
-  Array.from(Array(maxN / gap + 1).keys())
-    .map((val) => val * gap || 1)
-    .forEach((t, ti) => {
-      if (!result.datasets.some((ds) => ds.label === t))
-        result.datasets.push({ label: t, data: [] });
+  tList.forEach((t, ti) => {
+    if (!result.datasets.some((ds) => ds.label === t))
+      result.datasets.push({ label: t, data: [] });
 
-      let threshold = t;
-      // if (t === "n") {
-      //   threshold = n
-      // } else if (t === "1") {
-      //   threshold = 1
-      // } else {
-      //   threshold = parseInt(t.slice(2))
-      // }
+    let threshold = t;
+    // if (t === "n") {
+    //   threshold = n
+    // } else if (t === "1") {
+    //   threshold = 1
+    // } else {
+    //   threshold = parseInt(t.slice(2))
+    // }
 
-      let time = 0;
-      for (let i = 0; i < round; i++) {
-        const strassenWithThresholdStart = performance.now();
-        matrixMultiplicationStrassen(n, n, n, matrix1, matrix2, threshold);
-        const strassenWithThresholdEnd = performance.now();
-        time += strassenWithThresholdEnd - strassenWithThresholdStart;
-      }
-      result.datasets[ti].data.push(time / round);
-      console.log(`${t}-${threshold}-${n}-${time / round}`);
-    });
-}
+    let time = 0;
+    for (let i = 0; i < round; i++) {
+      const strassenWithThresholdStart = performance.now();
+      matrixMultiplicationStrassen(n, n, n, matrix1, matrix2, threshold);
+      const strassenWithThresholdEnd = performance.now();
+      time += strassenWithThresholdEnd - strassenWithThresholdStart;
+    }
+    result.datasets[ti].data.push(time / round);
+    console.log(`${t}-${threshold}-${n}-${time / round}`);
+  });
+});
 
 fs.writeFileSync("report/data.js", `export default ${JSON.stringify(result)}`);
